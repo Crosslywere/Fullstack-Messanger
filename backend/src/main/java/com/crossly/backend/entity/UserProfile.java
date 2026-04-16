c76449a54e17cf4c10c740b0c39cd3cd6eebdd38
Crosslywere <ogboruj@outlook.com>
2026-04-16 13:46:25 +0100

    Created UserProfile class

---
+++ backend/src/main/java/com/crossly/backend/entity/UserProfile.java
@@ -0,0 +1,36 @@
+package com.crossly.backend.entity;
+
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@Entity
+@Table(name = "USER_PROFILES")
+public class UserProfile implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder.Default
+    private List<GrantedAuthority> authorities = List.of();
+
+}
