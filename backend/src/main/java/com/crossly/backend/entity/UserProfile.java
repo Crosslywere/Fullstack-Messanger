542b5d23ca9cf8b14cc3c052a122cf7777d8830d
Crosslywere <ogboruj@outlook.com>
2026-04-17 15:56:59 +0100

    Added constructor lombok anotations

--- backend/src/main/java/com/crossly/backend/entity/UserProfile.java
+++ backend/src/main/java/com/crossly/backend/entity/UserProfile.java
@@ -12,13 +12,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @Builder
 @Data
 @Entity
 @Table(name = "USER_PROFILES")
+@NoArgsConstructor
 public class UserProfile implements UserDetails {
 
     @Id
