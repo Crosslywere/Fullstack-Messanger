f350d0d76264ac896d12e516761793c3522e7d28
Crosslywere <ogboruj@outlook.com>
2026-04-19 00:48:24 +0100

    Created data transfer objects - Created auth request dto - Created auth response dto - Created error response dto

---
+++ backend/src/main/java/com/crossly/backend/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.crossly.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private int status;
+
+    private String message;
+
+}
