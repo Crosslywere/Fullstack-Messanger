0f963509af5da62451ffb28b3a26358aa5dda1a7
Crosslywere <ogboruj@outlook.com>
2026-04-20 22:36:32 +0100

    Updated backend api to detect a missing connection

--- frontend/src/utilities/BackendApi.tsx
+++ frontend/src/utilities/BackendApi.tsx
@@ -11,14 +11,20 @@ const api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.data !== undefined) {
+    if (!error.response) {
+      if (import.meta.env.VITE_APP_BACKEND_URL) {
+        toast.error(`Please check your connection!`);
+      } else {
+        // TODO Remove in production
+        toast.error(`No connection to the backend was defined in .env`);
+      }
+    } else if (error.response?.data !== undefined) {
       const { status, message } = error.response.data as ErrorResponse;
       toast.error(`Error ${status} - ${message}`, {
         closeOnClick: true,
         closeButton: false,
       });
     }
-    return Promise.reject(error);
   },
 );
 
