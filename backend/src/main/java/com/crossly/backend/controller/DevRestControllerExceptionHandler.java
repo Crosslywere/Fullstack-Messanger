227101f58d3eb9770e83c4da082cd1cc6acf133d
Crosslywere <ogboruj@outlook.com>
2026-04-20 09:47:53 +0100

    Added exception handler for MethodArgumentNotValidException

--- backend/src/main/java/com/crossly/backend/controller/DevRestControllerExceptionHandler.java
+++ backend/src/main/java/com/crossly/backend/controller/DevRestControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.crossly.backend.controller;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +21,15 @@ public class DevRestControllerExceptionHandler {
                 .message(e.getMessage())
                 .build();
         return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        var response = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("The request was not valid!")
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
