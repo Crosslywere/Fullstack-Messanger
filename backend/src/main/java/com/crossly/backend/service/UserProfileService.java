00d4bfa8cf47cf8cfcdc417ffcb3053d06d5f024
Crosslywere <ogboruj@outlook.com>
2026-04-23 20:37:57 +0100

    Createdd extract token utility

--- backend/src/main/java/com/crossly/backend/service/UserProfileService.java
+++ backend/src/main/java/com/crossly/backend/service/UserProfileService.java
@@ -1,9 +1,12 @@
 package com.crossly.backend.service;
 
+import java.util.Arrays;
+
 import com.crossly.backend.dto.AuthRequest;
 import com.crossly.backend.dto.AuthResponse;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserProfileService {
@@ -14,20 +17,31 @@ public interface UserProfileService {
 
     AuthResponse loginUser(AuthRequest request, HttpServletResponse response);
 
-    default void removeJwtToken(HttpServletResponse response) {
+    public static void removeJwtToken(HttpServletResponse response) {
         var cookie = new Cookie(JWT_COOKIE_NAME, null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
         response.addCookie(cookie);
     }
 
-    default void attachTokenCookie(HttpServletResponse response, String token) {
+    public static void attachTokenCookie(HttpServletResponse response, String token) {
         var cookie = new Cookie(JWT_COOKIE_NAME, token);
         cookie.setMaxAge(60 * 60);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         response.addCookie(cookie);
+    }
+
+    public static String extractToken(HttpServletRequest request) {
+        var cookies = request.getCookies();
+        if (cookies == null)
+            return null;
+        return Arrays.asList(cookies).stream()
+                .filter(cookie -> cookie.getName().equals(JWT_COOKIE_NAME))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
     }
 
 }
